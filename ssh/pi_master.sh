179194c0144e7c9f8dc1f17baf50b6ec8d7bda64
enrikiko <enrikiko_91@hotmail.com>
2020-10-03 22:36:06 +0200

    Perform option instead think fast down why

--- ssh/pi_master.sh
+++ ssh/pi_master.sh
@@ -1,3 +1,7 @@
-IP=$(curl -s https://5nwdav0wk9.execute-api.eu-central-1.amazonaws.com/dev/get_ip)
+#IP=$(curl -s https://5nwdav0wk9.execute-api.eu-central-1.amazonaws.com/dev/get_ip)
 #echo "Connecting to $IP... "
-ssh -p 3388 ubuntu@${IP//\"}
+#ssh -p 3388 ubuntu@${IP//\"}
+IP=$(curl -s -X POST -H "x-api-key: ${ROUTE53_KEY}" ${ROUTE53_URL})
+echo "Connecting to $IP... "
+ssh -p 3388 ubuntu@${IP}
+echo "Disconected from $IP"
